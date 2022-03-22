8a8787587e183d34dbdfe381db8d2d50559c27c7
nilodornellas <iannilo01@gmail.com>
2022-03-21 22:12:21 -0300

    Adionado a função de capturar a imagem e armazenar em um arquivo para manipular.

--- lib/widgets/image_input.dart
+++ lib/widgets/image_input.dart
@@ -1,13 +1,33 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class ImageInput extends StatefulWidget {
-  const ImageInput({Key? key}) : super(key: key);
+  final Function? onSelectImage;
+  const ImageInput({Key? key, this.onSelectImage}) : super(key: key);
 
   @override
   State<ImageInput> createState() => _ImageInputState();
 }
 
 class _ImageInputState extends State<ImageInput> {
+  File? _storedImage;
+
+  _takePicture() async {
+    final ImagePicker _picker = ImagePicker();
+    XFile imageFile = await _picker.pickImage(
+      source: ImageSource.camera,
+      maxWidth: 600,
+    ) as XFile;
+
+    setState(() {
+      _storedImage = File(imageFile.path);
+    });
+
+    //widget.onSelectImage!(...);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Row(
@@ -19,7 +39,9 @@ class _ImageInputState extends State<ImageInput> {
             border: Border.all(width: 1, color: Colors.grey),
           ),
           alignment: Alignment.center,
-          child: Text('Nenhuma imagem!'),
+          child: _storedImage != null
+              ? Image.file(_storedImage!)
+              : Text('Nenhuma imagem!'),
         ),
         SizedBox(width: 10),
         Expanded(
