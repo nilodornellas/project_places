f4166914527288c44f777243aae557c0b5288986
nilodornellas <iannilo01@gmail.com>
2022-03-29 20:46:07 -0300

    Adicionado o provider de GreatPlaces.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:great_places/providers/great_places.dart';
 import 'package:great_places/screens/place_form_screen.dart';
 import 'package:great_places/utils/app_routes.dart';
+import 'package:provider/provider.dart';
 import './screens/places_list_screen.dart';
 
 void main() {
@@ -11,19 +13,22 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Great Places',
-      theme: ThemeData(
-        colorScheme: ThemeData().colorScheme.copyWith(
-              secondary: Colors.amber,
-              primary: Colors.indigo,
-            ),
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+    return ChangeNotifierProvider(
+      create: (ctx) => GreatPlaces(),
+      child: MaterialApp(
+        title: 'Great Places',
+        theme: ThemeData(
+          colorScheme: ThemeData().colorScheme.copyWith(
+                secondary: Colors.amber,
+                primary: Colors.indigo,
+              ),
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: PlacesListScreen(),
+        routes: {
+          AppRoutes.PLACE_FORM: (ctx) => PlaceFormScreen(),
+        },
       ),
-      home: PlacesListScreen(),
-      routes: {
-        AppRoutes.PLACE_FORM: (ctx) => PlaceFormScreen(),
-      },
     );
   }
 }
