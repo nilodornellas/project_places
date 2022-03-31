700fe2e4c8dc6b89f53a3f7acff03d208c18ad98
nilodornellas <iannilo01@gmail.com>
2022-03-30 23:00:16 -0300

    Criando a tela de seleção do lugar no map.

---
+++ lib/screens/map_screen.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:great_places/models/place.dart';
+
+class MapScreen extends StatefulWidget {
+  final PlaceLocation initialLocation;
+  const MapScreen({
+    Key? key,
+    this.initialLocation =
+        const PlaceLocation(latitude: 37.419857, longitude: -122.078827),
+  }) : super(key: key);
+
+  @override
+  State<MapScreen> createState() => _MapScreenState();
+}
+
+class _MapScreenState extends State<MapScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Selecione...'),
+      ),
+      body: GoogleMap(
+        initialCameraPosition: CameraPosition(
+          target: LatLng(
+            widget.initialLocation.latitude,
+            widget.initialLocation.longitude,
+          ),
+          zoom: 13,
+        ),
+      ),
+    );
+  }
+}
